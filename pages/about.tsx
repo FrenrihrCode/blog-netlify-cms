92992ae7241c469d7afd29147c03ef556c5131cb
NOTCompile <51174683+NOTCompile@users.noreply.github.com>
2021-10-09 19:44:37 -0500

    feat/Design_About_Me

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,33 @@
 import { ownerAboutData } from "../lib/data";
 import Styles from "../styles/modules/About.module.scss";
+import Image from "next/image";
 
 const About = () => {
   const aboutData = ownerAboutData;
 
   return (
     <div className={Styles.about}>
-      <h1 className={Styles["about-title"]}>Sobre mí</h1>
-      <p>{aboutData.intro}</p>
-      <p>{aboutData.desc}</p>
-      {aboutData.extra && <p>{aboutData.extra}</p>}
-      <span>Puedes hechar un vistazo a mis redes sociales:</span>
+      <div className={Styles["about-img"]}>
+        <Image 
+          src="/img/img_about_avatar.png"
+          layout="fill"
+        ></Image>
+      </div>
+      <div className={Styles["about-content"]}>
+        <h1 className={Styles["about-title"]}>Sobre mí</h1>
+        <p>{aboutData.intro}</p>
+        <p>{aboutData.desc}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Dolores quam reprehenderit incidunt unde non vero eum, aliquam 
+          commodi alias provident iusto illum quis? Fuga quasi aliquid 
+          deserunt asperiores earum repudiandae.Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Dolores quam reprehenderit incidunt
+          unde non vero eum, aliquam commodi alias provident iusto illum quis?
+          Fuga quasi aliquid deserunt asperiores earum repudiandae.
+        </p>
+        {aboutData.extra && <p>{aboutData.extra}</p>}
+        <span>Puedes hechar un vistazo a mis redes sociales:</span>
+      </div>
     </div>
   );
 };
