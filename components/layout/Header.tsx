c40d068951c0f7bcff73fedcef8928b71b78b006
NOTCompile <51174683+NOTCompile@users.noreply.github.com>
2021-10-12 19:34:53 -0500

    feat: sidebar_burger_menu

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -3,7 +3,7 @@ import { slide as MenuSlide } from "react-burger-menu";
 import { useState } from "react";
 import { Owner } from "../../interfaces/owner.interface";
 import { MenuItem } from "../../interfaces/layout.interface";
-import { MenuIcon } from "../icons";
+import { ContactIcon, HomeIcon, MenuIcon, PersonIcon } from "../icons";
 import { Sidebar } from "../menu/Sidebar";
 import { useRouter } from "next/router";
 
@@ -21,6 +21,14 @@ export const Header = ({
   const goTo = (to: string) => {
     router.push(to);
     setIsOpen(false);
+  };
+  const getIcon = (icon: "home" | "person" | "contact") => {
+    const icons = {
+      home: HomeIcon({ color: "white" }),
+      person: PersonIcon({ color: "white" }),
+      contact: ContactIcon({ color: "white" }),
+    };
+    return icons[icon];
   };
 
   const menuStyles = {
@@ -52,11 +60,11 @@ export const Header = ({
       fill: "#373a47",
     },
     bmItemList: {
-      color: "#b8b7ad",
-      padding: "0.8em",
+      color: "#ffffff",
+      padding: "1.5em",     
     },
     bmItem: {
-      display: "inline-block",
+      display: "flex",
     },
     bmOverlay: {
       background: "rgba(0, 0, 0, 0.5)",
@@ -64,6 +72,7 @@ export const Header = ({
       top: "0",
       left: "0",
     },
+    
   };
 
   return (
@@ -88,11 +97,13 @@ export const Header = ({
         customBurgerIcon={false}
       >
         <Sidebar owner={owner} items={[]} isForMenu={true}></Sidebar>
-        {items.map((item) => (
-          <button key={item.slug} onClick={() => goTo(item.slug)}>
-            {item.name}
-          </button>
+        <div className="sidebar-content">
+          {items.map((item) => (
+            <button  className="sidebar-content-button" key={item.slug} onClick={() => goTo(item.slug)}>
+              <span aria-label="icon">{getIcon(item.icon)}</span>{item.name}
+            </button>  
         ))}
+        </div>
       </MenuSlide>
     </>
   );
